74ac0bd67e18744463a56b626a9d92e4799b024d
Uschipanzer <52177341+Uschipanzer@users.noreply.github.com>
2020-03-26 15:03:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -201,9 +201,9 @@
      * @enum {Object}
      */
     Runner.keycodes = {
-        JUMP: { '38': 1, '32': 1 },  // Up, spacebar
-        DUCK: { '40': 1 },  // Down
-        RESTART: { '13': 1 }  // Enter
+        JUMP: { '38': 1, '32': 1, '87': 1 }, // Up, spacebar, w
+        DUCK: { '40': 1, '83': 1 }, // Down, s
+        RESTART: { '13': 1 } // Enter
     };
 
 
